c03d41894eb30547968505389576df518ceb6c7b
Nicola <della90@msn.com>
2021-12-08 14:29:05 +0000

    Refactored day 8

--- src/main/kotlin/adventofcode/year2021/Day08.kt
+++ src/main/kotlin/adventofcode/year2021/Day08.kt
@@ -18,10 +18,10 @@ class Day08(input: String) : AbstractPuzzle(input) {
         return input
             .lines()
             .map { it.split(" | ") }
+            .map { (wires, display) -> Pair(wires.split(" "), display.split(" ")) }
             .sumOf { (wires, display) ->
-                val mappings = guessMappings(wires.split(" "))
+                val mappings = guessMappings(wires)
                 val digits = display
-                    .split(" ")
                     .map { decodeNumber(mappings, it) }
 
                 digits.reduce { acc, digit ->
@@ -31,103 +31,63 @@ class Day08(input: String) : AbstractPuzzle(input) {
             .toString()
     }
 
-    private fun decodeNumber(mappings: Map<Int, Char>, segments: String): Int {
-        val allSegments = mappings.values.toSet()
-        return when (segments.length) {
-            2 -> 1
-            3 -> 7
-            4 -> 4
-            7 -> 8
-            6 -> {
-                val missingSegment = (allSegments - segments.toSet()).first()
-                when (missingSegment) {
-                    mappings.getValue(3) -> 0
-                    mappings.getValue(2) -> 6
-                    mappings.getValue(4) -> 9
-                    else -> throw IllegalArgumentException()
-                }
-            }
-            5 -> {
-                val missingSegments = allSegments - segments.toSet()
-                when (missingSegments) {
-                    setOf(mappings.getValue(1), mappings.getValue(5)) -> 2
-                    setOf(mappings.getValue(1), mappings.getValue(4)) -> 3
-                    setOf(mappings.getValue(2), mappings.getValue(4)) -> 5
-                    else -> throw IllegalArgumentException()
-                }
-            }
-            else -> throw IllegalArgumentException()
-        }
+    private fun decodeNumber(wireMappings: Map<Int, Set<Char>>, segments: String): Int {
+        val s = segments.toSet()
+        return wireMappings
+            .filterValues { it == s }
+            .firstNotNullOf { it.key }
     }
 
-    private fun guessMappings(wires: List<String>): Map<Int, Char> {
-        val wireMappings = hashMapOf<Int, Char>()
-        val segmentsByNumber = hashMapOf<Int, Set<Char>>()
-
-        val one = wires.first { it.length == 2 }
-        segmentsByNumber[1] = one.toSet()
-
-        // let's guess some random order for the two segments on the right. We'll fix them later
-        wireMappings[2] = one.first()
-        wireMappings[5] = one.last()
-
-        val seven = wires.first { it.length == 3 }
-        segmentsByNumber[7] = seven.toSet()
-        wireMappings[0] = (segmentsByNumber.getValue(7) - segmentsByNumber.getValue(1)).first()
-
-        val four = wires.first { it.length == 4 }
-        segmentsByNumber[4] = four.toSet()
-
-        // 9 is the only number that contains all segments in 7 and 4
-        val nine = wires.first {
-            it.toSet().containsAll(segmentsByNumber.getValue(7) + segmentsByNumber.getValue(4)) && it.length == 6
-        }
-        segmentsByNumber[9] = nine.toSet()
-
-        // 8 contains all segments
-        val eight = wires.first { it.length == 7 }
-        segmentsByNumber[8] = eight.toSet()
-
-        // the bottom left segment can be found subtracting 9 from 8
-        wireMappings[4] = (segmentsByNumber.getValue(8) - segmentsByNumber.getValue(9)).first()
-
-        // 0 contains all segments in 7 plus the bottom left segment
-        val zero = wires.first {
-            it.length == 6 && wireMappings[4] in it.toSet() && it.toSet().containsAll(segmentsByNumber.getValue(7))
-        }
-        segmentsByNumber[0] = zero.toSet()
-
-        // 0 and 9 differ only in two segments: bottom left (already known) and centre
-        wireMappings[3] = (segmentsByNumber.getValue(9) - segmentsByNumber.getValue(0)).first()
-
-        // 3 is the only digit that has all segments in 7 plus centre, and has five segments in total
-        val three = wires.first {
-            it.length == 5 && it.toSet().containsAll(segmentsByNumber.getValue(7) + wireMappings[3])
-        }
-        segmentsByNumber[3] = three.toSet()
-        wireMappings[6] = (segmentsByNumber.getValue(3) - wireMappings.values.toSet()).first()
-
-        // the only segment left is top left
-        wireMappings[1] = ("abcdefg".toSet() - wireMappings.values.toSet()).first()
-
+    private fun guessMappings(wires: List<String>): Map<Int, Set<Char>> {
         /*
-         * Now we need to fix the segment on the right that we guessed at the beginning. We can use digit
-         * 2 to find out what the bottom right segment is.
-         *
-         * To do that, we assume the original guess was right. If there is no digit that has a length of
-         * five segments and contains the top right segment, then it means our original guess was wrong
-         * and we have to swap the mappings.
+         * segments are:
+         * a == top
+         * b == top left
+         * c == top right
+         * d == centre
+         * e == bottom left
+         * f == bottom right
+         * g == bottom
          */
+        val one = wires.first { it.length == 2 }
+        val seven = wires.first { it.length == 3 }
+        val four = wires.first { it.length == 4 }
+        val eight = wires.first { it.length == 7 }
 
-        val two = wires.firstOrNull {
-            it.length == 5 && it.toSet().containsAll(segmentsByNumber.getValue(8) - wireMappings[1] - wireMappings[5])
-        }
-        if (two == null) {
-            val tmp = wireMappings.getValue(2)
-            wireMappings[2] = wireMappings.getValue(5)
-            wireMappings[5] = tmp
-        }
+        val c = one.minByOrNull { segment ->
+            wires.count { segment in it }
+        }!!
+        val f = one.first { it != c }
 
-        return wireMappings
+        val two = wires.first { f !in it }
+        val a = seven.first { it !in one }
+
+        val five = wires
+            .filter { it.length == 5 }
+            .first { c !in it }
+
+        val six = wires
+            .filter { it.length == 6 }
+            .first { c !in it }
+
+        val three = wires
+            .filter { it.length == 5 }
+            .first { a in it && c in it && f in it }
+
+        val e = two.first { it !in three }
+
+        val nine = wires
+            .filter { it.length == 6 }
+            .first { e !in it }
+
+        val zero = wires
+            .filter { it.length == 6 }
+            .first {
+                it != six && it != nine
+            }
+
+        return listOf(zero, one, two, three, four, five, six, seven, eight, nine)
+            .mapIndexed { index, segments -> index to segments.toSet() }
+            .toMap()
     }
 }
